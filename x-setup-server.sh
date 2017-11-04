a36428280ff4ae32521f49baf76e444603ee67b1
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2017-11-04 22:33:49 +0200

    Changed provision scripts location

--- x-setup-server.sh
+++ x-setup-server.sh
@@ -15,14 +15,13 @@ cd $(dirname $0/)
 SUBNET_PREFIX=172.15.0.
 DEFAULT_HOST_SECRET=""
 # Loading project options and overriding default variables
-if [ -e "../.env" ];  then source ../.env; fi
+if [ -e ".env" ];  then source .env; fi
 SERVER=${SUBNET_PREFIX}254
 
 printf "Building, launching and configuring Zabbix server. "
-cd ..
 docker-compose build &> /dev/null
 docker-compose up -d &> /dev/null
 echo "Done."
 echo "Installing and configuring host's agent."
 echo $DEFAULT_HOST_METADATA
-./scripts/setup-agent.sh -s ${SERVER} -l ${SUBNET_PREFIX}1 --hostname $(hostname -f) -m "Linux ${DEFAULT_HOST_SECRET}" --enable-docker-module
+./x-setup-agent.sh -s ${SERVER} -l ${SUBNET_PREFIX}1 --hostname $(hostname -f) -m "Linux ${DEFAULT_HOST_SECRET}" --enable-docker-module
