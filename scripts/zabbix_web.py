2ca7672b1aa0e12d001f297640b7fd7dffe0d226
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2017-12-15 18:22:43 +0200

    Added --unique options for zabbix_web.py script

--- scripts/zabbix_web.py
+++ scripts/zabbix_web.py
@@ -5,6 +5,7 @@ from urlparse import urlparse
 
 CMD_DISCOVERY = "discovery"
 ALL = -1
+WEB_LIST_TITLE_NAME="web"
 
 parser = argparse.ArgumentParser(prog="./%s"%(os.path.basename(sys.argv[0])), description="External script for Zabbix agent")
 parser.add_argument(CMD_DISCOVERY, help="URL discovery command")
@@ -12,6 +13,7 @@ parser.add_argument("--debug", action="store_true", help="Enable debug mode")
 parser.add_argument("--config", default="url_list.json", help="JSON list of web urls")
 parser.add_argument("--protocol", default=ALL, choices=[str(ALL),"http","https"], help="Limit url discovery mode by url protocol")
 parser.add_argument("--priority", default=ALL, type=int, help="Limit url discovery mode by url checking priority")
+parser.add_argument("--unique", action="store_true", help="Make distinct selection from web part of config (only disimilar server name)")
 
 args = parser.parse_args()
 options = vars(args)
@@ -29,7 +31,9 @@ class Web:
         self.debug = options["debug"]
         self.protocol = options["protocol"]
         self.priority = options["priority"]
-        self.url_list = self.read_config()["web"]
+        self.unique = options["unique"]
+        config = self.read_config()
+        self.url_list = config[WEB_LIST_TITLE_NAME] if WEB_LIST_TITLE_NAME in config else []
         if CMD_DISCOVERY in options:
             self.command = CMD_DISCOVERY
 
@@ -49,9 +53,10 @@ class Web:
                 continue
             if priority != ALL and int(item["priority"]) != priority:
                 continue
+            # if self.unique and url.scheme + "://" + url.netloc in
             result.append({
                 "{#DESCRIPTION}": item["name"],
-                "{#URL}": item["url"],
+                "{#URL}": item["url"] if not self.unique else url.scheme + "://" + url.netloc,
                 "{#HOSTNAME}": url.netloc.split(":")[0]
             })
         return json.dumps({"data": result})
